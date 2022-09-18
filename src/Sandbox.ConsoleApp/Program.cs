99ac27cabca9de6020f0f7db2382a3ca9485aec1
data-miner00 <data-miner00@users.noreply.github.com>
2022-09-18 13:56:24 +0800

    feat: Added sqlite data access

--- src/Sandbox.ConsoleApp/Program.cs
+++ src/Sandbox.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace Sandbox.ConsoleApp
 {
     using Sandbox.FSharp.Library;
+    using Sandbox.SQLite;
     using Sandbox.VisualBasic.Library;
 
     internal class Program
@@ -14,6 +15,38 @@
                 FirstName = "first",
                 LastName = "last",
             };
+
+            LoadPeopleList();
+
+            for (var i = 0; i < 2; i++)
+            {
+                var firstName = Console.ReadLine();
+                var lastName = Console.ReadLine();
+
+                SavePerson(firstName!, lastName!);
+            }
+        }
+
+        private static void LoadPeopleList()
+        {
+            var people = SQLiteDataAccess.LoadPeople();
+            int i = 1;
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{i++}. FN: {person.FirstName}, LN: {person.LastName}");
+            }
+        }
+
+        private static void SavePerson(string firstName, string lastName)
+        {
+            var person = new Person
+            {
+                FirstName = firstName,
+                LastName = lastName,
+            };
+
+            SQLiteDataAccess.SavePerson(person);
         }
     }
 }
---
+++ src/Sandbox.SQLite/SQLiteDataAccess.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Sandbox.VisualBasic.Library;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.SQLite
+{
+    public class SQLiteDataAccess
+    {
+        public static List<Person> LoadPeople()
+        {
+            using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = conn.Query<Person>("SELECT * FROM Person;", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void SavePerson(Person person)
+        {
+            using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var parameter = new DynamicParameters();
+
+                conn.Execute("INSERT INTO Person (FirstName, LastName) values (@FirstName, @LastName)", person);
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
