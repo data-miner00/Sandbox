cc71c195971ec003a459c84bae00af21fb3331c6
data-miner00 <data-miner00@users.noreply.github.com>
2023-02-20 13:36:12 +0800

    feat: Added switch and fs example

---
+++ src/Sandbox.ConsoleApp/FileSystem.cs
@@ -0,0 +1,23 @@
+﻿namespace Sandbox.ConsoleApp
+{
+    using System;
+    using Sandbox.Core;
+
+    internal class FileSystem : IDemo
+    {
+        public void Demo()
+        {
+            using var reader = File.OpenText("sometext.txt");
+
+            var line = reader.ReadLine();
+
+            while (line != null)
+            {
+                // do something with line
+                Console.WriteLine(line);
+
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/Sandbox.ConsoleApp/Switch.cs
@@ -0,0 +1,23 @@
+﻿namespace Sandbox.ConsoleApp
+{
+    using System;
+
+    /// <summary>
+    /// Switch statement that has the cpability to check for conditions.
+    /// </summary>
+    internal static class Switch
+    {
+        public static void Demo()
+        {
+            switch (10)
+            {
+                case var d when d >= 10:
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine();
+                    break;
+            }
+        }
+    }
+}
