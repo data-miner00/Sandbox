59d42a7691f51726a10f58d358515e193ad5cd3b
data-miner00 <data-miner00@users.noreply.github.com>
2023-05-21 17:54:47 +0800

    feat: Added ipcheck

---
+++ src/Sandbox.IpCheck/Program.fs
@@ -0,0 +1,22 @@
+﻿namespace Sandbox.Ipcheck
+
+module Program =
+
+    open System.Net
+    open System.Net.NetworkInformation
+
+    [<EntryPoint>]
+    let main argv =
+        if NetworkInterface.GetIsNetworkAvailable() then
+            printfn "Current IP Addresses: "
+
+            let hostname = Dns.GetHostName()
+            let host = Dns.GetHostEntry(hostname)
+
+            for address in host.AddressList do
+                printf $"\t{address}"
+        else
+            printfn "No Network Connection"
+        
+        0
+
