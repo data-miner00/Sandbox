d36d58d82b96ca315cc72f4063d05a55b605c385
data-miner00 <data-miner00@users.noreply.github.com>
2023-06-04 19:32:01 +0800

    feat: Added Aws workspace

---
+++ src/Sandbox.Aws/EventPublisher.cs
@@ -0,0 +1,44 @@
+﻿namespace Sandbox.Aws
+{
+    using System.Collections.Generic;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+    using Amazon.SQS;
+    using Amazon.SQS.Model;
+    using Sandbox.Core.Events;
+
+    internal class EventPublisher
+    {
+        private readonly AmazonSQSClient sqsClient;
+
+        public EventPublisher(AmazonSQSClient sqsClient)
+        {
+            this.sqsClient = sqsClient;
+        }
+
+        public async Task PublishCustomerCreatedEvent(CustomerCreatedEvent @event)
+        {
+            // To get the url without hardcoding
+            var queueUrlResponse = await this.sqsClient.GetQueueUrlAsync("customers");
+
+            var sendMessageRequest = new SendMessageRequest
+            {
+                QueueUrl = queueUrlResponse.QueueUrl,
+                MessageBody = JsonSerializer.Serialize(@event),
+                MessageAttributes = new Dictionary<string, MessageAttributeValue>
+                {
+                    {
+                        "MessageType", new MessageAttributeValue
+                        {
+                            DataType = "String",
+                            StringValue = nameof(CustomerCreatedEvent),
+                        }
+                    },
+                },
+                DelaySeconds = 3,
+            };
+
+            var response = await this.sqsClient.SendMessageAsync(sendMessageRequest);
+        }
+    }
+}
---
+++ src/Sandbox.Aws/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Sandbox.Aws
+{
+    using Amazon;
+    using Amazon.SQS;
+
+    internal static class Program
+    {
+        internal static async Task Main(string[] args)
+        {
+            // many ways to instantiate a sqs client
+            var sqsClient = new AmazonSQSClient(RegionEndpoint.AFSouth1);
+
+            var sqsClient2 = new AmazonSQSClient(new AmazonSQSConfig
+            {
+                RegionEndpoint = RegionEndpoint.APNortheast1,
+            });
+        }
+    }
+}
