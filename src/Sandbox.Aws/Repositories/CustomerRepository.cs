ece97b0153dc5778d2891e5f4f934e94c602d2ca
data-miner00 <data-miner00@users.noreply.github.com>
2023-06-09 22:24:11 +0800

    docs: Added repository with DynamoDB

---
+++ src/Sandbox.Aws/Repositories/CustomerRepository.cs
@@ -0,0 +1,77 @@
+﻿namespace Sandbox.Aws.Repositories;
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Sandbox.Library.FSharp.Dtos;
+
+internal class CustomerRepository
+{
+    private const string TableName = "customers";
+    private readonly IAmazonDynamoDB dynamoDb;
+
+    public CustomerRepository(IAmazonDynamoDB dynamoDb)
+    {
+        this.dynamoDb = dynamoDb;
+    }
+
+    public async Task<bool> CreateAsync(CustomerDto customer)
+    {
+        var customerAsJson = JsonSerializer.Serialize(customer);
+        var customerAsAttributes = Document.FromJson(customerAsJson).ToAttributeMap();
+
+        var createItemRequest = new PutItemRequest
+        {
+            TableName = TableName,
+            Item = customerAsAttributes,
+        };
+
+        var response = await this.dynamoDb.PutItemAsync(createItemRequest);
+
+        return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+    }
+
+    public async Task<CustomerDto?> GetByIdAsync(Guid id)
+    {
+        var getItemRequest = new GetItemRequest
+        {
+            TableName = TableName,
+            Key = new Dictionary<string, AttributeValue>
+            {
+                { "pk", new AttributeValue { S = id.ToString() } },
+                { "sk", new AttributeValue { S = id.ToString() } },
+            },
+        };
+
+        var response = await this.dynamoDb.GetItemAsync(getItemRequest);
+
+        if (response.Item.Count == 0)
+        {
+            return null;
+        }
+
+        var itemAsDocument = Document.FromAttributeMap(response.Item);
+        return JsonSerializer.Deserialize<CustomerDto>(itemAsDocument.ToJson());
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var deleteItemRequest = new DeleteItemRequest
+        {
+            TableName = TableName,
+            Key = new Dictionary<string, AttributeValue>
+            {
+                { "pk", new AttributeValue { S = id.ToString() } },
+                { "sk", new AttributeValue { S = id.ToString() } },
+            },
+        };
+
+        var response = await this.dynamoDb.DeleteItemAsync(deleteItemRequest);
+
+        return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+    }
+}
---
+++ src/Sandbox.Library.FSharp/Dtos/CustomerDto.fs
@@ -0,0 +1,11 @@
+﻿namespace Sandbox.Library.FSharp.Dtos
+
+type CustomerDto =
+    struct
+        val PartitionKey: string
+        val SortKey: string
+        val FirstName: string
+        val LastName: string
+        val Age: int
+    end
+
