740bf0f524685bc94a0dfcbe4b9e87dd561eef7e
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-11 18:51:35 +0800

    chore: Add silo

---
+++ src/Sandbox.Silo/DynamicXml.cs
@@ -0,0 +1,46 @@
+﻿namespace Sandbox.Silo
+{
+    using System.Collections;
+    using System.Dynamic;
+    using System.Runtime.CompilerServices;
+    using System.Xml.Linq;
+
+    public class DynamicXml : DynamicObject, IEnumerable
+    {
+        public static implicit operator string(DynamicXml xml) => xml.xml.Value;
+
+        private dynamic xml;
+
+        public DynamicXml(string fileName)
+        {
+            this.xml = XDocument.Load(fileName);
+        }
+
+        public DynamicXml(dynamic xml)
+        {
+            this.xml = xml;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in this.xml.Elements())
+            {
+                yield return new DynamicXml(item);
+            }
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object? result)
+        {
+            var xml = this.xml.Element(binder.Name);
+
+            if (xml == null)
+            {
+                result = new DynamicXml(xml);
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
---
+++ src/Sandbox.Silo/LazyFind.cs
@@ -0,0 +1,22 @@
+﻿namespace Sandbox.Silo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class LazyFind
+    {
+        private static IEnumerable<int> Find(IEnumerable<int> list, Func<int, bool> f)
+        {
+            foreach (var item in list)
+            {
+                if (f(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
