6209822be71ca1f5eb65d70e92633de0d48446c8
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-13 20:43:26 +0800

    docs: Add jObject and jArray examples

---
+++ src/Sandbox.Newtonsoft/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Sandbox.Newtonsoft.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string City { get; set; }
+    }
+}
--- src/Sandbox.Newtonsoft/Serialization.cs
+++ src/Sandbox.Newtonsoft/Serialization.cs
@@ -2,12 +2,36 @@
 {
     using global::Newtonsoft;
     using global::Newtonsoft.Json;
+    using global::Newtonsoft.Json.Linq;
+    using Sandbox.Newtonsoft.Models;
 
-    public class Serialization
+    public static class Serialization
     {
-        public void SerializeExample()
+        public static void SerializeExample()
         {
             JsonConvert.SerializeObject(new { Hello = "hallo" });
+
+            var jsonString = "{\"name\":\"John\":,\"age\":30,\"city\":\"New York\"}";
+            var person = JsonConvert.DeserializeObject<Person>(jsonString);
+        }
+
+        public static void DeserializeIntoJObject()
+        {
+            var jsonString = "{\"name\":\"John\":,\"age\":30,\"city\":\"New York\"}";
+
+            var jobject = JObject.Parse(jsonString);
+
+            var name = (string)jobject["name"];
+            var age = (int)jobject["age"];
+            var city = (string)jobject["city"];
+        }
+
+        public static void DeserializeIntoJArray()
+        {
+            var jsonString = "[{\"name\":\"John\":,\"age\":30,\"city\":\"New York\"}]";
+
+            var jarray = JArray.Parse(jsonString);
+            var query = jarray.Where(x => (int)x["age"] == 5).ToList();
         }
     }
 }
