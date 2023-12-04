a6ae0fcc0ea6dd16561a0d71e50aeb82f79afd42
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-04 21:27:38 +0800

    docs: Add array examples

---
+++ src/Sandbox.Silo/CloneExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace Sandbox.Silo
+{
+    internal static class CloneExtension
+    {
+        public static T Clone<T>(this T obj)
+            where T : class, new()
+        {
+            var newObj = new T();
+            var properties = obj.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(obj, null);
+                property.SetValue(newObj, value);
+            }
+
+            return newObj;
+        }
+    }
+}
