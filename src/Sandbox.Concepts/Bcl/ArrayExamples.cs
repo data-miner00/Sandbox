7143b4db09a813205b73e13c0496c47774b2da94
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-05 22:40:00 +0800

    docs: Add collections

---
+++ src/Sandbox.Concepts/Bcl/ArrayExamples.cs
@@ -0,0 +1,58 @@
+﻿namespace Sandbox.Concepts.Bcl
+{
+    using System;
+    using System.Collections;
+
+    /// <summary>
+    /// Some example operations involving arrays.
+    /// </summary>
+    internal static class ArrayExamples
+    {
+        /// <summary>
+        /// Resize and copy nth element of an array.
+        /// </summary>
+        public static void Arrays()
+        {
+            var someArray = new[] { 1, 2, 3, 4 };
+            var emptyArray = new int[0] { };
+
+            Array.Resize(ref emptyArray, 3);
+            Array.Copy(someArray, emptyArray, 3);
+        }
+
+        /// <summary>
+        /// Multidimensional array examples.
+        /// </summary>
+        public static void Multidimension()
+        {
+            int[,] twoDimension =
+            {
+                { 1, 2 },
+                { 3, 4 },
+                { 5, 6 },
+            };
+
+            Console.WriteLine(twoDimension.Rank);
+
+            for (var outer = twoDimension.GetLowerBound(0); outer <= twoDimension.GetUpperBound(0); outer++)
+            {
+                for (var inner = twoDimension.GetLowerBound(1); inner <= twoDimension.GetUpperBound(1); inner++)
+                {
+                    Console.WriteLine($"{outer}, {inner}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Bit array examples
+        /// </summary>
+        public static void Bits()
+        {
+            // Initialize bit array and initialize to true
+            var ba = new BitArray(5, true);
+
+            var bytes = new byte[5] { 1, 2, 3, 5, 3 };
+            var casted = new BitArray(bytes);
+        }
+    }
+}
---
+++ src/Sandbox.Concepts/Bcl/CollectionsExamples.cs
@@ -0,0 +1,109 @@
+﻿namespace Sandbox.Concepts.Bcl
+{
+    using System.Collections;
+
+    /// <summary>
+    /// System.Collections examples class.
+    /// </summary>
+    internal static class CollectionsExamples
+    {
+        /// <summary>
+        /// Use for comparing objects?.
+        /// </summary>
+        public static void DefaultComparer()
+        {
+            var str1 = new string('a', 3);
+            var str2 = new string('b', 3);
+
+            var result = Comparer.DefaultInvariant.Compare(str1, str2);
+            Console.WriteLine($"{str1} == {str2} : {result}");
+        }
+
+        public static void HashtableExample()
+        {
+            var cars = new Hashtable
+            {
+                { "green", "Land Rover" },
+                { "artic", "Frontier" },
+                { "palomino", "Land Rover" },
+                { "red", "Mustang" },
+            };
+
+            cars.Add("pink", "Urus");
+
+            foreach (DictionaryEntry entry in cars)
+            {
+                Console.WriteLine($"Key: {entry.Key}, Value: {entry.Value}");
+            }
+
+            ICollection keys = cars.Keys;
+
+            // Add method throws an exception if key is existing.
+            try
+            {
+                cars.Add("pink", "Mustang");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Implemented with 2 arrays. One to store keys another to store value.
+        /// </summary>
+        public static void SortedListExamples()
+        {
+            var sl = new SortedList
+            {
+                { 3, "A" },
+                { 5, "B" },
+                { 1, "C" },
+                { 2, "D" },
+                { 8, "E" },
+                { 6, "F" },
+            };
+
+            Console.WriteLine("Count: " + sl.Count);
+            Console.WriteLine("Capacity: " + sl.Capacity);
+
+            for (var i = 0; i < sl.Count; i++)
+            {
+                Console.WriteLine("{0}: {1}", sl.GetKey(i), sl.GetByIndex(i));
+            }
+        }
+
+        public static void QueueExamples()
+        {
+            // Implemented with circular array
+            var queue = new Queue();
+
+            queue.Enqueue("a");
+            queue.Enqueue("b");
+            queue.Enqueue("c");
+            queue.Enqueue("d");
+            queue.Enqueue("e");
+
+            Console.WriteLine($"{queue.Dequeue()}");
+            Console.WriteLine($"{queue.Peek()}");
+
+            foreach (var obj in queue)
+            {
+                Console.WriteLine("{0} ", obj);
+            }
+        }
+
+        public static void StackExamples()
+        {
+            var stack = new Stack();
+
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+            stack.Push("d");
+            stack.Push("e");
+
+            Console.WriteLine($"{stack.Pop()}");
+        }
+    }
+}
