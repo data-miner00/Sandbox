0bb35f348b41fbead3e94d5f954a068991e41bd5
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-09 21:38:23 +0800

    feat: Add working MSSQL

---
+++ src/Sandbox.MSSQL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Sandbox.MSSQL
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public sealed class AppDbContext : DbContext
+    {
+        private const string ConnectionString = @"Server=DESKTOP-KL3VEUU;Database=Testing;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ src/Sandbox.MSSQL/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace Sandbox.MSSQL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public sealed class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+    }
+}
