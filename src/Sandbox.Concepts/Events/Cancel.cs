84ac0aec17b5866449def99814e7d812a3104e07
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-10 12:57:51 +0800

    docs: Cancel token source

---
+++ src/Sandbox.Concepts/Events/Cancel.cs
@@ -0,0 +1,12 @@
+﻿namespace Sandbox.Concepts.Events
+{
+    internal static class Cancel
+    {
+        public static void Main()
+        {
+            var cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (_, _) => cts.Cancel();
+        }
+    }
+}
