3852a1f81a73e9f4b23a38ebc33f369b68327a69
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-10 10:02:25 +0800

    docs: Add more collections

--- src/Sandbox.Concepts/Bcl/CollectionsExamples.cs
+++ src/Sandbox.Concepts/Bcl/CollectionsExamples.cs
@@ -1,7 +1,11 @@
 ﻿namespace Sandbox.Concepts.Bcl
 {
     using System.Collections;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Collections.ObjectModel;
+    using System.Collections.Specialized;
 
     /// <summary>
     /// System.Collections examples class.
@@ -74,6 +78,9 @@
             }
         }
 
+        /// <summary>
+        /// Examples of enqueue, dequeue and peek for Queue.
+        /// </summary>
         public static void QueueExamples()
         {
             // Implemented with circular array
@@ -94,6 +101,9 @@
             }
         }
 
+        /// <summary>
+        /// Examples of push and pop operations on a stack.
+        /// </summary>
         public static void StackExamples()
         {
             var stack = new Stack();
@@ -107,6 +117,9 @@
             Console.WriteLine($"{stack.Pop()}");
         }
 
+        /// <summary>
+        /// Examples of adding even numbers to a hash set.
+        /// </summary>
         public static void HashSetExamples()
         {
             HashSet<int> evenNumbers = [];
@@ -114,6 +127,148 @@
             for (var i = 0; i < 10; i++)
             {
                 evenNumbers.Add(i * 2);
+            }
+        }
+
+        /// <summary>
+        /// Examples of ConcurrentBag unordered collection.
+        /// </summary>
+        public static void ConcurrentBagExamples()
+        {
+            var cb = new ConcurrentBag<int>();
+
+            var bagAddTasks = new List<Task>();
+
+            for (var i = 0; i < 50; i++)
+            {
+                bagAddTasks.Add(Task.Run(() => cb.Add(i)));
+            }
+
+            Task.WaitAll([.. bagAddTasks]);
+
+            Console.WriteLine("All tasks have completed");
+
+            var bagConsumeTasks = new List<Task>();
+
+            int itemsInBag = 0;
+
+            while (!cb.IsEmpty)
+            {
+                bagConsumeTasks.Add(Task.Run(() =>
+                {
+                    if (cb.TryTake(out var item))
+                    {
+                        Console.WriteLine(item);
+                        itemsInBag++;
+                    }
+                }));
+            }
+
+            Task.WaitAll([.. bagConsumeTasks]);
+            Console.WriteLine($"There were {itemsInBag} items in bag");
+
+            if (cb.TryPeek(out var item))
+            {
+                Console.WriteLine("Should not have items, found: " + item);
+            }
+        }
+
+        public static void ConcurrentQueueExamples()
+        {
+            var cq = new ConcurrentQueue<int>();
+
+            for (var i = 0; i < 10_000; i++)
+            {
+                cq.Enqueue(i);
+            }
+
+            if (cq.TryPeek(out var item))
+            {
+                Console.WriteLine("Try peek succes with " + item);
+            }
+
+            Console.WriteLine();
+
+            var outerSum = 0;
+
+            // An action to consume the cq
+            var action = () =>
+            {
+                var localSum = 0;
+
+                while (cq.TryDequeue(out var localValue))
+                {
+                    localSum += localValue;
+                }
+
+                Interlocked.Add(ref outerSum, localSum);
+            };
+
+            // Start 4 concurrent consuming actions
+            Parallel.Invoke(action, action, action, action);
+
+            Console.WriteLine("outerSum = {0}, should be 49995000", outerSum);
+        }
+
+        public static async Task ConcurrentStackExamples()
+        {
+            var items = 10_000;
+
+            var cs = new ConcurrentStack<int>();
+
+            var pusher = () =>
+            {
+                for (var i = 0; i < items; i++)
+                {
+                    cs.Push(i);
+                }
+            };
+
+            pusher();
+
+            if (cs.TryPeek(out var item))
+            {
+                Console.WriteLine("TryPeek() saw {0} on top of the stack", item);
+            }
+
+            // Clear the stack
+            cs.Clear();
+
+            if (cs.IsEmpty)
+            {
+                Console.WriteLine("The stack is empty");
+            }
+
+            var pushAndPop = () =>
+            {
+                Console.WriteLine("Task started on {0}", Task.CurrentId);
+
+                for (var i = 0; i < items; i++)
+                {
+                    cs.Push(i);
+                }
+
+                for (var i = 0; i < items; ++i)
+                {
+                    cs.TryPop(out var item);
+                }
+
+                Console.WriteLine("Task ended on {0}", Task.CurrentId);
+            };
+
+            // Spin up five concurrent tasks of the action
+            var tasks = new Task[5];
+
+            for (var i = 0; i < tasks.Length; ++i)
+            {
+                tasks[i] = Task.Factory.StartNew(pushAndPop);
+            }
+
+            await Task.WhenAll(tasks);
+
+            if (cs.IsEmpty)
+            {
+                await Console.Out.WriteLineAsync("Items all processed!");
             }
         }
     }
