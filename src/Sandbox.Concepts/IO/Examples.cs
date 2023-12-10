f92a470dd231a09c3499e875721149711f697cdc
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-10 17:48:55 +0800

    docs: Add some IO

---
+++ src/Sandbox.Concepts/IO/Examples.cs
@@ -0,0 +1,118 @@
+﻿namespace Sandbox.Concepts.IO
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+
+    /// <summary>
+    /// The examples for I/O operations.
+    /// </summary>
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+    internal class Examples
+    {
+        public static void WriteStringsToFile(string filename)
+        {
+            File.WriteAllText(filename, "My string contents");
+        }
+
+        /// <summary>
+        /// Read all the contents in a file into a big string.
+        /// </summary>
+        /// <param name="filename">The file npath.</param>
+        public static void ReadContentFromFile(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("Not found");
+                return;
+            }
+
+            var content = File.ReadAllText(filename);
+        }
+
+        /// <summary>
+        /// Can use this method to validate a given file path.
+        /// </summary>
+        public static void GetInvalidCharsForPath()
+        {
+            var invalidChars = Path.GetInvalidPathChars();
+
+            foreach (var ch in invalidChars)
+            {
+                Console.Write(ch + " ");
+            }
+        }
+
+        /// <summary>
+        /// Writes each string element in an array into newline.
+        /// </summary>
+        /// <param name="filename">The file path.</param>
+        public static void UsingWriteAllLines(string filename)
+        {
+            var lines = new[] { "hello", "world", "bello", "borld" };
+            File.WriteAllLines(filename, lines, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// Append the text content at the end of the given file.
+        /// </summary>
+        /// <param name="filename">The file path.</param>
+        public static void AppendText(string filename)
+        {
+            var appendText = "This is extra text" + Environment.NewLine;
+            File.AppendAllText(filename, appendText, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// Each individual line is loaded into an array of string.
+        /// </summary>
+        /// <param name="filename">The file path.</param>
+        public static void UsingReadAllLines(string filename)
+        {
+            var lines = File.ReadAllLines(filename, Encoding.UTF8);
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        /// <summary>
+        /// Read the contents into an array of bytes.
+        /// </summary>
+        /// <param name="filename">The file path.</param>
+        public static void UsingReadAllBytes(string filename)
+        {
+            var bytes = File.ReadAllBytes(filename);
+
+            foreach (var byyte in bytes)
+            {
+                Console.WriteLine($"{byyte}, ");
+            }
+        }
+
+        /// <summary>
+        /// Getting useful information for a file.
+        /// </summary>
+        public static void FileInfoExamples()
+        {
+            var filePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            var fileInfo = new FileInfo(filePath);
+
+            if (fileInfo.Exists)
+            {
+                Console.WriteLine(
+                    string.Format(
+                        "File Information: {0}, {1} bytes, last modified on {2} - Full path: {3}",
+                        fileInfo.Name,
+                        fileInfo.Length,
+                        fileInfo.LastWriteTime,
+                        fileInfo.FullName));
+            }
+        }
+
+        private string GetDebuggerDisplay() => this.ToString() ?? nameof(Examples);
+    }
+}
---
+++ src/Sandbox.Concepts/IO/Streams.cs
@@ -0,0 +1,96 @@
+﻿namespace Sandbox.Concepts.IO
+{
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Examples for working with streams.
+    /// </summary>
+    internal static class Streams
+    {
+        /// <summary>
+        /// Example for using File.OpenWrite.
+        /// </summary>
+        /// <param name="filename">The file path.</param>
+        public static void WriteFileStream(string filename)
+        {
+            using var wfs = File.OpenWrite(filename);
+
+            var data = "hello\nworld\n\nwelcome";
+            var bytes = Encoding.UTF8.GetBytes(data);
+
+            wfs.Write(bytes, 0, bytes.Length);
+        }
+
+        /// <summary>
+        /// Example for using File.OpenRead.
+        /// </summary>
+        /// <param name="filename">The file path.</param>
+        public static void ReadFileStream(string filename)
+        {
+            using var rfs = File.OpenRead(filename);
+            var buf = new byte[1024];
+            int c;
+
+            while ((c = rfs.Read(buf, 0, buf.Length)) > 0)
+            {
+                Console.WriteLine(Encoding.UTF8.GetString(buf, 0, c));
+            }
+        }
+
+        /// <summary>
+        /// Example for using MemoryStream with bytes and StreamReader.
+        /// </summary>
+        /// <param name="filename">The file path.</param>
+        public static void ReadMemoryStream(string filename)
+        {
+            var contentBuf = File.ReadAllBytes(filename);
+            using var memoryStream = new MemoryStream(contentBuf);
+            using var textReader = new StreamReader(memoryStream);
+
+            var line = string.Empty;
+
+            while ((line = textReader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        /// <summary>
+        /// Example for downloading an image from the internet.
+        /// </summary>
+        /// <param name="imageUrl">The url for the image.</param>
+        /// <returns>The task.</returns>
+        public static async Task DownloadImage(string imageUrl)
+        {
+            using var httpClient = new HttpClient();
+            var imageBytes = await httpClient.GetByteArrayAsync(imageUrl);
+
+            using var fs = new FileStream("img.jpg", FileMode.Create);
+            fs.Write(imageBytes, 0, imageBytes.Length);
+        }
+
+        /// <summary>
+        /// Example for reading an image as bytes.
+        /// </summary>
+        /// <param name="filename">The file path.</param>
+        public static void ViewImage(string filename)
+        {
+            using var fs = new FileStream(filename, FileMode.Open);
+
+            int c, i = 0;
+
+            while ((c = fs.ReadByte()) != -1)
+            {
+                Console.WriteLine("{0:X2}", c);
+                i++;
+
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
