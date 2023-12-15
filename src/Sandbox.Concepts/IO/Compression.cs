14a48e4ac912fd1d829d0f82e23c0a6f7730c913
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-15 22:44:43 +0800

    docs: Add compression

---
+++ src/Sandbox.Concepts/IO/Compression.cs
@@ -0,0 +1,25 @@
+﻿namespace Sandbox.Concepts.IO
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO.Compression;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal static class Compression
+    {
+        public static void ZipDirExample()
+        {
+            var startPath = @".\toZip";
+            var zipPath = @".\compressed.zip";
+            var extractPath = @"\.extract";
+
+            // Zipping a directory
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+
+            // Unzipping to a directory
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+    }
+}
