304fe620ddb683db538d5dff227e9f750c0d05af
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-18 14:11:33 +0800

    docs: Add xml read write and serialization

---
+++ src/Sandbox.Concepts/IO/XML/Book.cs
@@ -0,0 +1,27 @@
+﻿namespace Sandbox.Concepts.IO.XML
+{
+    using System;
+    using System.Xml;
+    using System.Xml.Serialization;
+
+    public sealed class Book
+    {
+        /// <summary>
+        /// Setting isNullable to false tells the serializer that
+        /// the XML will no appear if Title is set to null.
+        /// </summary>
+        [XmlElement(IsNullable = false)]
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        [XmlAttribute]
+        public string Genre { get; set; }
+
+        [XmlAttribute]
+        public string ISBN { get; set; }
+
+        [XmlAttribute("publicationdate")]
+        public DateTime PublicationDate { get; set; }
+    }
+}
---
+++ src/Sandbox.Concepts/IO/XML/MemoryEfficient.cs
@@ -0,0 +1,89 @@
+﻿namespace Sandbox.Concepts.IO.XML;
+
+using System;
+using System.Xml;
+
+public static class MemoryEfficient
+{
+    /// <summary>
+    /// It provides a fast, non-cached, forward-only way to read XML files.
+    /// </summary>
+    public static void UsingXmlReader()
+    {
+        using var fileStream = File.OpenText("books.xml");
+        using var reader = XmlReader.Create(fileStream);
+
+        while (reader.Read())
+        {
+            switch (reader.NodeType)
+            {
+                case XmlNodeType.Element:
+                    Console.WriteLine($"Start element: {reader.Name}. Has Attributes? {reader.HasAttributes}");
+                    break;
+                case XmlNodeType.Text:
+                    Console.WriteLine($"Inner text: {reader.Value}");
+                    break;
+                case XmlNodeType.CDATA:
+                    Console.WriteLine("This is a CDATA");
+                    break;
+                case XmlNodeType.Whitespace:
+                    Console.WriteLine("This is a whitespace");
+                    break;
+                case XmlNodeType.SignificantWhitespace:
+                    Console.WriteLine("This is a significant whitespace");
+                    break;
+                case XmlNodeType.EndElement:
+                    Console.WriteLine($"End element: {reader.Name}");
+                    break;
+                default:
+                    Console.WriteLine($"Other element type: {reader.NodeType}");
+                    break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// It provides a fast, non-cached, forward-only way to generate XML files.
+    /// </summary>
+    public static void UsingXmlWriter(string filename)
+    {
+        using var writer = XmlWriter.Create(filename);
+
+        // <catalog>
+        writer.WriteStartElement("catalog");
+
+        // <book>
+        writer.WriteStartElement("book");
+        writer.WriteAttributeString(null, "id", null, "bk101");
+
+        // <author>
+        writer.WriteStartElement("author");
+        writer.WriteString("Susan Cheong");
+
+        // </author>
+        writer.WriteEndElement();
+
+        // <title>
+        writer.WriteStartElement("title");
+        writer.WriteString("Good reads");
+
+        // </title>
+        writer.WriteEndElement();
+
+        // <publish_date>
+        writer.WriteStartElement("publish_date");
+        writer.WriteValue(new DateTime(2000, 01, 01, 00, 00, 00, DateTimeKind.Utc));
+
+        // </publish_date>
+        writer.WriteEndElement();
+
+        // </book>
+        writer.WriteEndElement();
+
+        // </catalog>
+        writer.WriteEndElement();
+
+        // Create the file
+        writer.Flush();
+    }
+}
