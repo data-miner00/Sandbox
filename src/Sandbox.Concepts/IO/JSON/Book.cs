e731a5ebc111c07ab8c9459076a79bb0a0efdd3f
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-18 22:48:13 +0800

    chore: Migrate Json related stuff from Authr

--- src/Sandbox.Concepts/IO/JSON/Book.cs
+++ src/Sandbox.Concepts/IO/JSON/Book.cs
@@ -1,19 +1,70 @@
 ﻿namespace Sandbox.Concepts.IO.JSON
 {
     using System;
+    using System.Text.Json.Serialization;
 
-    public sealed class Book
+    /// <summary>
+    /// A sample book implementation with JsonPropertyAttribute.
+    /// Additionally implements the IEquatable interface and implements operator.
+    /// </summary>
+    public class Book : IEquatable<Book>
     {
+        [JsonPropertyName("title")]
         public string Title { get; set; }
 
+        [JsonPropertyName("price")]
         public decimal Price { get; set; }
 
+        [JsonPropertyName("genre")]
         public string Genre { get; set; }
 
+        [JsonPropertyName("isbn")]
         public string ISBN { get; set; }
 
+        [JsonPropertyName("publication_date")]
         public DateTime PublicationDate { get; set; }
 
+        [JsonPropertyName("tags")]
         public IList<string> Tags { get; set; }
+
+        #region Other stuffs
+        public static bool operator !=(Book left, Book right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator ==(Book left, Book right)
+        {
+            if (left != right)
+            {
+                if (left is not null)
+                {
+                    return left.Equals(right);
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return int.MaxValue;
+        }
+
+        public virtual bool Equals(Book? other)
+        {
+            // very crude equavalency
+            return this.Title == other?.Title;
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Book);
+        }
+        #endregion
     }
 }
---
+++ src/Sandbox.Concepts/IO/JSON/BookRecord.cs
@@ -0,0 +1,22 @@
+﻿namespace Sandbox.Concepts.IO.JSON;
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// Book implemented as a record with Json serializable fields.
+/// </summary>
+/// <param name="Title">The title of the book.</param>
+/// <param name="Price">The price of the book.</param>
+/// <param name="Genre">The genre of the book.</param>
+/// <param name="ISBN">The ISBN code of the book.</param>
+/// <param name="PublicationDate">The publication date.</param>
+/// <param name="Tags">The tags associated with it.</param>
+public record BookRecord(
+    [property: JsonPropertyName("title")] string Title,
+    [property: JsonPropertyName("price")] decimal Price,
+    [property: JsonPropertyName("genre")] string Genre,
+    [property: JsonPropertyName("isbn")] string ISBN,
+    [property: JsonPropertyName("publication_date")] DateTime PublicationDate,
+    [property: JsonPropertyName("tags")] IList<string> Tags);
