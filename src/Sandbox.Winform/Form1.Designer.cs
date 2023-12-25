508f69648e68a07afde31c30d3a742a8a74581a5
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-25 17:02:05 +0800

    chore: Add winform project

---
+++ src/Sandbox.Winform/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Sandbox.Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Label();
+            this.textBox1 = new TextBox();
+            this.button1 = new Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new Point(126, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new Size(68, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Change me";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new Point(126, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new Point(246, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new Size(124, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Update text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += this.button1_Click;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new SizeF(7F, 15F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
