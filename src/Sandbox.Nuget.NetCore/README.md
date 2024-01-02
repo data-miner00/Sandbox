c64dd4abbb60088ea6c7c68a405e676ce001ff7b
data-miner00 <data-miner00@users.noreply.github.com>
2024-01-02 19:38:52 +0800

    feat: Added nuget netcore project

---
+++ src/Sandbox.Nuget.NetCore/README.md
@@ -0,0 +1,3 @@
+﻿# Sandbox.Nuget.NetCore
+
+This is a sample repository for testing with nuget release with .Net Core
---
+++ src/Sandbox.Nuget.NetCore/Sample.cs
@@ -0,0 +1,10 @@
+﻿namespace Sandbox.Nuget.NetCore
+{
+    /// <summary>
+    /// An example class.
+    /// </summary>
+    public static class Sample
+    {
+        public const string Greetings = "Hello from NetCore Nuget";
+    }
+}
