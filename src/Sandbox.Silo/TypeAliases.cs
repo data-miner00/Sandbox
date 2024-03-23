451644a4fb5bc3c14e46a83b99819cf9322e05e5
data-miner00 <data-miner00@users.noreply.github.com>
2024-03-23 08:17:20 +0800

    docs: Add type alias

---
+++ src/Sandbox.Silo/TypeAliases.cs
@@ -0,0 +1,6 @@
+﻿#pragma warning disable SA1200 // Using directives should be placed correctly
+global using BandPass = (int Min, int Max);
+global using Letters = (string Author, string Content, int Year);
+global using MyInt = int;
+global using Range = System.Tuple<int, int>;
+#pragma warning restore SA1200 // Using directives should be placed correctly
