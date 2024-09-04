7fbd0c4380fcdbf0f3f29547c8ce242c9643cf61
data-miner00 <data-miner00@users.noreply.github.com>
2024-09-04 20:37:21 +0800

    vendor: Install System.IO.Hashing

---
+++ src/Sandbox.Experiment/Algra.cs
@@ -0,0 +1,20 @@
+﻿namespace Sandbox.Experiment;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Hashing;
+
+public class Algra
+{
+    public static void Demo()
+    {
+        var crc32 = new Crc32();
+
+        crc32.Append(Encoding.ASCII.GetBytes("Hello world"));
+
+        Console.WriteLine(crc32.GetCurrentHash());
+    }
+}
