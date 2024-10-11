91a988e82a104ea605b4f358163decb073bfe119
data-miner00 <data-miner00@users.noreply.github.com>
2024-10-11 23:16:51 +0800

    docs: Add garbage collection example

---
+++ src/Sandbox.Silo/GarbageCollection.cs
@@ -0,0 +1,25 @@
+﻿namespace Sandbox.Silo;
+
+using System;
+
+/// <summary>
+/// Whether an object is garbage collected.
+/// Refer to <see href="https://stackoverflow.com/questions/15460362/how-to-tell-if-an-object-has-been-garbage-collected">this post</see>.
+/// </summary>
+public class GarbageCollection
+{
+    public sealed record Dog(string Name);
+
+    public static void Demo()
+    {
+        Dog dog = new Dog("Bowser");
+
+        WeakReference dogRef = new WeakReference(dog);
+        Console.WriteLine(dogRef.IsAlive);
+
+        dog = null;
+        GC.Collect();
+
+        Console.WriteLine(dogRef.IsAlive);
+    }
+}
