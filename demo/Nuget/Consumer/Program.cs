2a24a1b744a1da2e53a8682a183cf90b1f6eb479
Shaun Chong <data-miner00@users.noreply.github.com>
2025-08-11 22:29:48 +0800

    chore: Add .net core consumer project

---
+++ demo/Nuget/Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox.Nuget.NetCore.EmbeddedResource;
+
+namespace Sandbox.Nuget.NetCore.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sample.Resource);
+        }
+    }
+}
