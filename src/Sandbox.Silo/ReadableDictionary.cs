436f65eaadc23698850226c694adc9dc7dea2c94
Shaun Chong <data-miner00@users.noreply.github.com>
2025-10-02 20:25:30 +0800

    docs: Add readable dictionary

---
+++ src/Sandbox.Silo/ReadableDictionary.cs
@@ -0,0 +1,33 @@
+﻿namespace Sandbox.Silo;
+
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Demonstrates a readable dictionary by giving the name for the key and value.
+/// </summary>
+internal static class ReadableDictionary
+{
+    public static void Demo()
+    {
+        var authDict = new MyAuthorDictionary();
+
+        authDict.Add(author: "Anthony", title: "Who took my cheese");
+
+        Console.WriteLine("Anthony has: {0}", authDict["Anthony"]);
+    }
+}
+
+file sealed class MyAuthorDictionary : Dictionary<string, string>
+{
+    public new void Add(string author, string title)
+    {
+        base.Add(author, title);
+    }
+
+    public new string this[string title]
+    {
+        get { return base[title]; }
+        set { base[title] = value; }
+    }
+}
