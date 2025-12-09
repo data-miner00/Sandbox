0e6e1bb1a168bcb303dc443f76c26dc5d093d9e6
Shaun Chong <data-miner00@users.noreply.github.com>
2025-12-09 18:25:04 +0800

    fix: Sync update

--- src/Sandbox.ConsoleApp/Program.cs
+++ src/Sandbox.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace Sandbox.ConsoleApp
 {
     using System;
+    using System.Text;
     using Sandbox.Concepts.Bcl.Threading;
     using Sandbox.Nuget.NetCore;
 
@@ -15,18 +16,60 @@
         /// <param name="args">The command line arguments.</param>
         public static void Main(string[] args)
         {
-            //SendingEmail.SendEmail("ben@contoso.com", "noreply@contoso.com");
-            Monitors.Example();
-            Console.WriteLine(Sample.Greetings);
-            var bo = Role.Administrator < Role.Moderator;
-        }
-    }
+            var data = "hello, world";
 
-    enum Role
-    {
-        Administrator,
-        Moderator,
-        User,
-        Guest,
+            var binaryData = GetCharBytes(data);
+
+            PrintBits(binaryData);
+
+            Console.WriteLine();
+        }
+
+        public static bool[] ToEightBitBoolArray(this int value)
+        {
+            if (value < 0 || value > 255)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Value must be between 0 and 255.");
+            }
+
+            bool[] bits = new bool[8];
+            for (int i = 0; i < 8; i++)
+            {
+                bits[7 - i] = (value & (1 << i)) != 0;
+            }
+
+            return bits;
+        }
+
+        private static bool[] GetCharBytes(string input)
+        {
+            // return input.Length.ToEightBitBoolArray();
+
+            var bytes = Encoding.ASCII.GetBytes(input);
+
+            var result = new bool[bytes.Length * 8];
+
+            for (int j = 0; j < bytes.Length; j++)
+            {
+                var bits = ((int)bytes[j]).ToEightBitBoolArray();
+
+                for (int i = 0; i < bits.Length; i++)
+                {
+                    result[(j * 8) + i] = bits[i];
+                }
+            }
+
+            return result;
+        }
+
+        private static void PrintBits(bool[] bits)
+        {
+            foreach (var bit in bits)
+            {
+                Console.Write(bit ? '1' : '0');
+            }
+
+            Console.WriteLine();
+        }
     }
 }
