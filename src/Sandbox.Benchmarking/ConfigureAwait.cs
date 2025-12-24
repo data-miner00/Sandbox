cf321df009cdacb6cbf8e0193585696ffd204a18
Shaun Chong <data-miner00@users.noreply.github.com>
2025-12-24 19:37:15 +0800

    docs: Add configure await benchmark

---
+++ src/Sandbox.Benchmarking/ConfigureAwait.cs
@@ -0,0 +1,29 @@
+﻿namespace Sandbox.Benchmarking
+{
+    using BenchmarkDotNet.Attributes;
+
+    [MemoryDiagnoser]
+    public class ConfigureAwait
+    {
+        [Params(100, 1_000, 2_000)]
+        public int Size { get; set; }
+
+        [Benchmark]
+        public async Task WithoutConfigureAwaitFalse()
+        {
+            for (int i = 0; i < this.Size; i++)
+            {
+                await Task.Delay(1);
+            }
+        }
+
+        [Benchmark]
+        public async Task WithConfigureAwaitFalse()
+        {
+            for (int i = 0; i < this.Size; i++)
+            {
+                await Task.Delay(1).ConfigureAwait(false);
+            }
+        }
+    }
+}
