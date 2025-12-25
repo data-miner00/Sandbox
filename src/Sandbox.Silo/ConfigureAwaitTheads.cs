92f2c7f6519e02ec4315bcbae19ce98da11743c5
Shaun Chong <data-miner00@users.noreply.github.com>
2025-12-25 18:27:06 +0800

    docs: Added configure await conundrum

---
+++ src/Sandbox.Silo/ConfigureAwaitTheads.cs
@@ -0,0 +1,32 @@
+﻿namespace Sandbox.Silo;
+
+using System;
+
+public static class ConfigureAwaitTheads
+{
+    public static async Task ConundrumAsync()
+    {
+        Console.WriteLine("Thread ID: {0}", Environment.CurrentManagedThreadId);
+        Console.WriteLine("\n\n");
+        Console.WriteLine("Awaited with ConfigureAwait(true)");
+        await Task.Run(() =>
+        {
+            Console.WriteLine("Inside first task Thread ID: {0}", Environment.CurrentManagedThreadId);
+        }).ConfigureAwait(true);
+        Console.WriteLine("Thread ID: {0}", Environment.CurrentManagedThreadId);
+        Console.WriteLine("\n\n");
+        Console.WriteLine("Awaited with ConfigureAwait(false)");
+        await Task.Run(() =>
+        {
+            Console.WriteLine("Inside second task Thread ID: {0}", Environment.CurrentManagedThreadId);
+        }).ConfigureAwait(false);
+        Console.WriteLine("Thread ID: {0}", Environment.CurrentManagedThreadId);
+        Console.WriteLine("\n\n");
+        Console.WriteLine("Awaited NO ConfigureAwait");
+        await Task.Run(() =>
+        {
+            Console.WriteLine("Inside third task Thread ID: {0}", Environment.CurrentManagedThreadId);
+        });
+        Console.WriteLine("Thread ID: {0}", Environment.CurrentManagedThreadId);
+    }
+}
