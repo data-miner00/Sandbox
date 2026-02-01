34641855f6d6135a8fdfbe27af71956d4a7b41b8
Shaun Chong <data-miner00@users.noreply.github.com>
2026-02-02 00:18:02 +0800

    feat: Add net48 vs net10.0 benchmarks

---
+++ demo/Benchmarks/Net48Net10/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Benchmarks.Net48Net10
+{
+    using BenchmarkDotNet.Running;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MultipleWhere>();
+        }
+    }
+}
