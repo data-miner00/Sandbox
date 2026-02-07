ef0650014ecdb3f52f5e9bc605ac8e40c8c54c68
Shaun Chong <data-miner00@users.noreply.github.com>
2026-02-07 11:02:24 +0800

    feat: Added core project

---
+++ demo/Benchmarks/Core.Net10/JoinArray.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Benchmarks.Core.Net10;
+
+public static class JoinArray
+{
+    public static string JoinArray1<T>(T[] array)
+    {
+        var str = string.Empty;
+        foreach (var item in array)
+        {
+            str += item.ToString();
+        }
+
+        return str;
+    }
+
+    public static string JoinArray2<T>(T[] array)
+    {
+        var sb = new StringBuilder();
+        foreach (var item in array)
+        {
+            sb.Append(item.ToString());
+        }
+
+        return sb.ToString();
+    }
+
+    public static string JoinArray3<T>(T[] array)
+    {
+        return string.Join("", array);
+    }
+}
