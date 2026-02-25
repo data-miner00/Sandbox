ebe77f067a03dda2043b377a8edf1c22de4d81be
Shaun Chong <data-miner00@users.noreply.github.com>
2026-02-25 22:13:15 +0800

    feat: Add simple and strategy retries

--- demo/FaultHandling/ConsoleApp/Program.cs
+++ demo/FaultHandling/ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using ConsoleApp;
+using Polly;
+using Polly.Retry;
 using Serilog;
 
 var logger = new LoggerConfiguration()
@@ -6,10 +8,36 @@ var logger = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateLogger();
 
-var errorRate = 80;
+var errorRate = 60;
 
 var client = new DangerClient(errorRate, logger);
 
-client.Execute();
+// Use simple policy
+var policy = Policy.Handle<BarrierPostPhaseException>()
+    .Retry(10);
+
+policy.Execute(() =>
+{
+    client.Execute();
+});
+logger.Information("Congratulations, the program ended in a success.");
+
+// Use resilience pipeline and retry strategy
+var options = new RetryStrategyOptions
+{
+    ShouldHandle = new PredicateBuilder().Handle<BarrierPostPhaseException>(),
+    BackoffType = DelayBackoffType.Exponential,
+    UseJitter = true,  // Adds a random factor to the delay
+    MaxRetryAttempts = 10,
+    Delay = TimeSpan.FromSeconds(3),
+};
+
+var pipelineBuilder = new ResiliencePipelineBuilder().AddRetry(options);
+
+client = new DangerClient(errorRate, logger);
+pipelineBuilder.Build().Execute(() =>
+{
+    client.Execute();
+});
 
 logger.Information("Congratulations, the program ended in a success.");
