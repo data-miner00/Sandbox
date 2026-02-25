784c049bd6a0819f0319be27d14e56ea6b5d5e19
Shaun Chong <data-miner00@users.noreply.github.com>
2026-02-25 21:54:17 +0800

    chore: Add console app to test method resiliency

---
+++ demo/FaultHandling/ConsoleApp/DangerClient.cs
@@ -0,0 +1,33 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    internal class DangerClient
+    {
+        private readonly int errorRate;
+        private readonly ILogger logger;
+        private int callCount = 0;
+
+        public DangerClient(int errorRate, ILogger logger)
+        {
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(errorRate, 100);
+            ArgumentOutOfRangeException.ThrowIfLessThan(errorRate, 0);
+            ArgumentNullException.ThrowIfNull(logger);
+            this.errorRate = errorRate;
+            this.logger = logger.ForContext<DangerClient>();
+        }
+
+        public void Execute()
+        {
+            this.logger.Information("This method have been called {Times} times", ++this.callCount);
+
+            if (Random.Shared.Next(0, 100) < this.errorRate)
+            {
+                throw new BarrierPostPhaseException();
+            }
+        }
+    }
+}
---
+++ demo/FaultHandling/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp;
+using Serilog;
+
+var logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var errorRate = 80;
+
+var client = new DangerClient(errorRate, logger);
+
+client.Execute();
+
+logger.Information("Congratulations, the program ended in a success.");
