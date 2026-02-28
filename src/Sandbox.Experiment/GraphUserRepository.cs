ea8823b5aaee3a623c65b643da8c98d26c2182df
Shaun Chong <data-miner00@users.noreply.github.com>
2026-02-28 08:41:06 +0800

    feat: Add simple Azure Graph usage

---
+++ src/Sandbox.Experiment/GraphUserRepository.cs
@@ -0,0 +1,109 @@
+﻿namespace Sandbox.Experiment
+{
+    using Azure.Identity;
+    using Microsoft.Graph;
+    using Microsoft.Graph.Models;
+
+    internal class GraphUserRepository
+    {
+        private static readonly string[] SelectedFields = ["Id", "GivenName", "Surname", "CreatedDateTime"];
+
+        private readonly GraphServiceClient client;
+
+        public GraphUserRepository(GraphServiceClient client)
+        {
+            this.client = client;
+        }
+
+        public GraphUserRepository()
+        {
+            var creds = new ClientSecretCredential("tenantId", "clientId", "clientSecret");
+            this.client = new GraphServiceClient(creds);
+        }
+
+        public async Task<User> GetByIdAsync(string id)
+        {
+            try
+            {
+                var user = await this.client.Users[id].GetAsync(opt =>
+                {
+                    opt.QueryParameters.Select = SelectedFields;
+                });
+
+                return user;
+            }
+            catch (ServiceException ex)
+            {
+                await Console.Error.WriteLineAsync(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            try
+            {
+                var final = new List<User>();
+
+                var users = await this.client.Users.GetAsync(opt =>
+                {
+                    opt.QueryParameters.Select = SelectedFields;
+                });
+
+                final.AddRange(users.Value);
+
+                while (users.OdataNextLink != null)
+                {
+                    users = await this.client.Users.WithUrl(users.OdataNextLink).GetAsync();
+                    final.AddRange(users.Value);
+                }
+
+                return final;
+            }
+            catch (ServiceException ex)
+            {
+                await Console.Error.WriteLineAsync(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            try
+            {
+                await this.client.Users[user.Id].PatchAsync(user);
+            }
+            catch (ServiceException ex)
+            {
+                await Console.Error.WriteLineAsync(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task CreateAsync(User user)
+        {
+            try
+            {
+                await this.client.Users.PostAsync(user);
+            }
+            catch (ServiceException ex)
+            {
+                await Console.Error.WriteLineAsync(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            try
+            {
+                await this.client.Users[id].DeleteAsync();
+            }
+            catch (ServiceException ex)
+            {
+                await Console.Error.WriteLineAsync(ex.Message);
+                throw;
+            }
+        }
+    }
+}
