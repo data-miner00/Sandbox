3e52fa53eb10cfeefd10cf61cc5325f07b7ad1d2
Shaun Chong <data-miner00@users.noreply.github.com>
2026-06-28 21:21:23 +0800

    feat: Add unexpected cache behavior

---
+++ src/Sandbox.Cache/DisposableObject.cs
@@ -0,0 +1,36 @@
+﻿namespace Sandbox.Cache
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal class DisposableObject : IDisposable
+    {
+        private string id = Guid.NewGuid().ToString();
+        private bool isDisposed;
+        private object _lock = new object();
+        private int _count = 0;
+
+        public int Count => _count;
+
+        public void Execute()
+        {
+            lock (_lock)
+            {
+                _count++;
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose method called. " + id);
+
+            if (!isDisposed)
+            {
+                isDisposed = true;
+
+                Console.WriteLine($"{id} object disposed.");
+            }
+        }
+    }
+}
---
+++ src/Sandbox.Cache/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Sandbox.Cache
+{
+    using Microsoft.Extensions.Caching.Memory;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cacheOption = new MemoryCacheOptions
+            {
+                SizeLimit = 1024,
+            };
+
+            var cache = new MemoryCache(cacheOption);
+
+            Console.WriteLine($"Current cache count: {cache.Count}");
+
+            for (var i = 0; i < 20; ++i)
+            {
+                var randomInt = GetRandomOneToThree();
+
+                var @object = cache.GetOrCreate(
+                    randomInt,
+                    entry =>
+                    {
+                        entry.SlidingExpiration = TimeSpan.FromSeconds(5);
+                        entry.Size = 2;
+                        entry.RegisterPostEvictionCallback((key, value, reason, state) =>
+                        {
+                            if (value is IDisposable disposable)
+                            {
+                                disposable.Dispose();
+                            }
+                        });
+
+                        return new DisposableObject();
+                    });
+
+                Console.WriteLine($"Current cache count: {cache.Count}");
+
+                @object.Execute();
+
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }
+        }
+
+        static int GetRandomOneToThree()
+        {
+            var random = new Random();
+            return random.Next(1, 4);
+        }
+    }
+}
