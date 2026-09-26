aa6ecac976042d42d240a73a80235432f3f6cfe8
Shaun Chong <data-miner00@users.noreply.github.com>
2026-09-26 14:17:59 +0800

    chore: Scaffold data protection project

---
+++ src/Sandbox.DataProtection/Program.cs
@@ -0,0 +1,44 @@
+namespace Sandbox.DataProtection;
+
+using Swashbuckle.AspNetCore.Filters;
+using System.Reflection;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.EnableAnnotations();
+            c.ExampleFilters();
+        });
+        builder.Services.AddSwaggerExamplesFromAssemblies(Assembly.GetEntryAssembly());
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
